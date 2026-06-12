4be247384e0eff8e1faa16ffd9f0c998aed9929c
koushik2411 <koushikrk1324@gmail.com>
2026-06-12 20:26:44 +0530

    9th commit: Feature- updated crud features of comments

--- controllers/comment.controller.js
+++ controllers/comment.controller.js
@@ -34,9 +34,55 @@ export const getComments = async (req, res) => {
     }
 };
 
+// EDIT COMMENT
+export const updateComment = async (req, res) => {
+    try {
+        const comment = await CommentModel.findById(req.params.id);
+
+        if (!comment) {
+            return res.status(404).json({
+                message: "Comment not found",
+            });
+        }
+
+        if (comment.user.toString() !== req.user.id) {
+            return res.status(403).json({
+                message: "Unauthorized",
+            })
+
+        }
+
+        comment.text = req.body.text;
+
+        await comment.save();
+
+        res.status(200).json(comment);
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+};
+
 // DELETE COMMENT
 export const deleteComment = async (req, res) => {
     try {
+
+        const comment = await CommentModel.findById(req.params.id);
+
+        if (!comment) {
+            return res.status(404).json({
+                message: "Comment not found",
+            });
+        }
+
+        if (comment.user.toString() !== req.user.id) {
+            return res.status(403).json({
+                message: "Unauthorized",
+            });
+        }
+
         await CommentModel.findByIdAndDelete(
             req.params.commentId
         );
--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import authMiddleware from "../middlewares/authMiddleware.js";
-import { addComment, deleteComment, getComments } from "../controllers/comment.controller.js";
+import { addComment, deleteComment, getComments, updateComment } from "../controllers/comment.controller.js";
 
 const router = express.Router();
 
@@ -9,6 +9,9 @@ router.get("/:videoId", getComments);
 
 // Add comment
 router.post("/:videoId", authMiddleware, addComment);
+
+// Update comment
+router.put("/:commentId", authMiddleware, updateComment);
 
 // Delete comment
 router.delete("/:commentId", authMiddleware, deleteComment);
